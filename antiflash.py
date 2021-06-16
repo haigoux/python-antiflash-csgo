38c29eff1ec9ead225538100919313b0bf32671c
ylmonkey <73267715+ylmonkey@users.noreply.github.com>
2021-06-15 18:46:59 -0600

    Create antiflash.py

---
+++ antiflash.py
@@ -0,0 +1,14 @@
+import pymem
+import requests
+
+
+proc = pymem.Pymem("csgo.exe")
+client = pymem.process.module_from_name(proc.process_handle, "client.dll").lpBaseOfDll
+engine = pymem.process.module_from_name(proc.process_handle, "engine.dll" ).lpBaseOfDll
+
+offsets = requests.get('https://raw.githubusercontent.com/frk1/hazedumper/master/csgo.json').json()
+dwLocalPlayer = int(offsets['signatures']['dwLocalPlayer'])
+
+while True:
+    player= proc.read_int(client + dwLocalPlayer)
+    proc.write_int(player + int(offsets['netvars']['m_flFlashDuration']), 0)
